dcaecefe31a005948b25aca424ccbb3ded0a7604
ductv-dev <vietducdtu@gmail.com>
2026-04-04 16:34:27 +0700

    fix header section in toke page

--- container/token/sections/section-header.tsx
+++ container/token/sections/section-header.tsx
@@ -2,12 +2,14 @@
 
 import { ButtonNav } from "@/components/custom/button/button-navigation"
 import { ChevronLeft } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export const SectionHeader = () => {
+  const route = useRouter()
   return (
-    <div className="fixed top-0 w-full">
+    <div className="fixed top-0 z-30 w-full bg-white/50 backdrop-blur-2xl">
       <div className="flex w-full border-b border-gray-300 p-2.5">
-        <ButtonNav>
+        <ButtonNav onClick={() => route.back()}>
           <ChevronLeft />
         </ButtonNav>
       </div>
