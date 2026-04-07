a358a14a4b08bbeb08c0418591c91ffb9605e9bd
ductv-dev <vietducdtu@gmail.com>
2026-04-07 11:10:54 +0700

    fix ui history page

--- container/history/page.tsx
+++ container/history/page.tsx
@@ -1,7 +1,15 @@
+import { BookAlert } from "lucide-react"
+
 export const HistoryPage = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <h1 className="text-2xl font-bold">History Page</h1>
+    <div className="flex h-full w-full flex-col gap-2.5 p-2.5">
+      <h1 className="text-xl font-semibold">Hoạt động</h1>
+      <div className="flex w-full flex-col items-center justify-center gap-2.5 py-20">
+        <div className="flex h-14 w-14 items-center justify-center rounded-xl border-t border-black/10 bg-white text-blue-700 shadow-lg shadow-black/10">
+          <BookAlert strokeWidth={3} size={24} />
+        </div>
+        <p>Chưa có hoạt động nào</p>
+      </div>
     </div>
   )
 }
